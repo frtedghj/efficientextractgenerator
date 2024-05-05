2089becd20bfcf6bbf75870f75ff1bf72c9e56d8
frtedghj <gs1kadir910@hotmail.com>
2024-05-05 07:55:03 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-// Setup the client
-const aptosConfig = new AptosConfig({ network: APTOS_NETWORK });
-const aptos = new Aptos(aptosConfig);
+function groupAnagrams(strs) {
+  const map = new Map();
+  for (const str of strs) {
+    const sortedStr = str.split("").sort().join("");
+    if (!map.has(sortedStr)) map.set(sortedStr, []);
+    map.get(sortedStr).push(str);
+  }
+  return [...map.values()];
+}
